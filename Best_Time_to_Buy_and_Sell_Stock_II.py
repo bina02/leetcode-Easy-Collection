f69c4bcfe1702417a83ad668c938cc2b80a5be54
bina02 <gusqls246@gmail.com>
2026-02-25 12:38:27 +0900

    Best Time to Buy and Sell Stock II

---
+++ Best_Time_to_Buy_and_Sell_Stock_II.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        profit = 0
+
+        for i in range(1, len(prices)):
+            if prices[i-1] < prices[i]:
+                profit += prices[i] - prices[i-1]
+
+        return profit
+    
+prices = [7, 1, 6]
+solution = Solution()
+profit = solution.maxProfit(prices)
+print(profit)
